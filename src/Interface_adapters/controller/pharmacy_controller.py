fd1c5108adc6bc49d48e3f4773a2c8072fde2e2e
claudineclauna27-cmyk <claudineclauna27@gmail.com>
2026-07-14 00:25:45 -0400

    pharmacy_managment

--- src/Interface_adapters/controller/pharmacy_controller.py
+++ src/Interface_adapters/controller/pharmacy_controller.py
@@ -1,9 +1,9 @@
 from uscases.add_medicine import AddMedicineInput, AddMedicineUseCase  
 from src.uscases.delete_medicine import DeleteMedicineInput, DeleteMedicineUseCase
-from src.uscases.update_medicine import updateMedicineInput, UpdateMedicineUseCase
+from src.uscases.update_medicine import UpdateMedicineInput, UpdateMedicineUseCase
 from src.uscases.interfaces.medicine_repo import IMedicineRepository
 
-class MedicamentController:
+class MedicineController:
     def __init__(self, repo: IMedicineRepository):
         self.repo = repo
 
@@ -17,7 +17,7 @@ class MedicamentController:
         use_case = DeleteMedicineUseCase(self.repo)
         return use_case.execute(input_data)
 
-    def modifier(self, id: int, nom: str, prix: float, quantite: int, date_expiration=None):
-        input_data = DeleteMedicineInput(id=id, nom=nom, prix=prix, quantite=quantite, date_expiration=date_expiration)
-        use_case = DeleteMedicineUseCase(self.repo)
+    def update(self, id: int, nom: str, prix: float, quantite: int, date_expiration=None):
+        input_data = UpdateMedicineInput(id=id, nom=nom, prix=prix, quantite=quantite, date_expiration=date_expiration)
+        use_case = UpdateMedicineUseCase(self.repo)
         return use_case.execute(input_data)
--- src/entities/medicine.py
+++ src/entities/medicine.py
@@ -12,11 +12,11 @@ class Medicine:
 
     def __post_init__(self):
         if not self.nom or self.nom.strip() == "":
-            raise ValueError("Le nom du médicament ne peut pas être vide ou composé uniquement d'espaces.")
+            raise ValueError("The drug name cannot be empty or consist solely of spaces...")
         if self.prix < 0:
-            raise ValueError("Le prix du médicament ne peut pas être négatif.")
+            raise ValueError("The price of the drug cannot be negative.")
         if self.quantite < 0:
-            raise ValueError("La quantité du médicament ne peut pas être négative.")
+            raise ValueError("The quantity of the medication cannot be negativ.")
 
     def est_expire(self) -> bool:
         if self.date_expiration is None:
