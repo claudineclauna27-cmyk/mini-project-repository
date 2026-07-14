7a7e1fc09802a42fd548c7f3aa2d510b6040a95b
claudineclauna27-cmyk <claudineclauna27@gmail.com>
2026-07-13 23:14:28 -0400

    my final project

--- src/uscase/interface/Pharmacy_repo.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-from src.entities.medication import Medication
-
-class PharmacyRepo(ABC):
-    @abstractmethod
-    def save(self, medication:Medication) -> Medication:
-        ...
-
-    
